6f15ab42a3635b92fca8e60eff86218a12181b43
YeongJun_LAPTOP <youkj06@naver.com>
2016-04-20 12:52:22 +0900

    레이아웃 초기구성
    메인화면 구성, 구글맵 추가, 정보화면 추가

---
+++ app/src/main/java/mp3/mp3/info/InfoActivity.java
@@ -0,0 +1,32 @@
+package mp3.mp3.info;
+
+import android.os.Bundle;
+import android.support.percent.PercentFrameLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import mp3.mp3.R;
+
+/**
+ * Created by KimYeongJun on 2015-12-29.
+ */
+public class InfoActivity extends AppCompatActivity {
+
+    PercentFrameLayout back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_main);
+
+        back = (PercentFrameLayout) findViewById(R.id.info_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.slide_in_left2, R.anim.slide_out_right);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/mp3/mp3/main/MainActivity.java
@@ -0,0 +1,59 @@
+package mp3.mp3.main;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import mp3.mp3.R;
+import mp3.mp3.info.InfoActivity;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    public void GOINFO(View view) {
+        startActivity(new Intent(this, InfoActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left2);
+    }
+
+
+}
